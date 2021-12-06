62ff6f0fe8d1a86bd18f42556b762eb578bce1ff
guoan <1813850191@qq.com>
2021-12-06 15:11:52 +0800

    提交测试Jenkins001

--- src/main/java/com/springboot/admin/controller/TestController.java
+++ src/main/java/com/springboot/admin/controller/TestController.java
@@ -3,12 +3,15 @@ package com.springboot.admin.controller;
 
 import com.springboot.admin.service.UserService;
 import com.sun.tools.sjavac.comp.PubapiVisitor;
+import org.apache.ibatis.annotations.ResultMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
-@RestController("testJenkins")
+@RestController
+@RequestMapping("testJenkins")
 public class TestController {
 
     @Resource
